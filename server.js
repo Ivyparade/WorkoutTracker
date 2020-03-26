730f7a3cfe5fe6cab595818e1dcf58a05c342b19
Owen <owenedwards422@gmail.com>
2020-03-25 20:12:36 -0400

    added html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,8 +18,32 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/api/workouts", (err, res)=> {
+app.get("/api/workouts", (req, res) => {
 
+});
+
+app.get("/api/workouts/range", (req, res) => {
+
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+
+});
+
+app.post("/api/workouts", (req, res) => {
+
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/stats.html"));
 });
 
 app.listen(PORT, function() {
