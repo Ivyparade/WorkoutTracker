0b0af10e7cd794c9f8458439edf323dbd12ed405
Owen <owenedwards422@gmail.com>
2020-03-25 21:30:54 -0400

    added routes

--- server.js
+++ server.js
@@ -19,19 +19,27 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.get("/api/workouts", (req, res) => {
-
+    db.Workout.find({})
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
 });
 
 app.get("/api/workouts/range", (req, res) => {
-
+    db.Workout.find({})
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
 });
 
 app.put("/api/workouts/:id", (req, res) => {
-
+    db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {new: true, runValidators: true})
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
 });
 
 app.post("/api/workouts", (req, res) => {
-
+    db.Workout.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
 });
 
 app.get("/", (req, res) => {
